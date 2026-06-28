35db59247e1f4959264d0e64c508f8a254be71cd
Белов Дмитрий <108318627+tech-specialist@users.noreply.github.com>
2026-06-29 01:02:58 +0300

    All exercises from the 5th lesson and the home work.

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,5 +1,4 @@
 package tests;
-
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
@@ -16,22 +15,39 @@ public class LoginTest extends BaseTest {
     }
 
     @Test
-    public void checkIncorrectLogin() {
+    public void checkEmptyLogin() {
         loginPage.open();
         loginPage.login("","secret_sauce");
-
 
         assertTrue(loginPage.isErrorDisplayed());
         assertEquals(loginPage.getErrorText(), "Epic sadface: Username is required");
     }
 
     @Test
-    public void checkIncorrectPassword() {
+    public void checkEmptyPasswordLogin() {
         loginPage.open();
-        loginPage.login("","secret_sauce");
+        loginPage.login("standard_user","");
 
 
         assertTrue(loginPage.isErrorDisplayed());
-        assertEquals(loginPage.getErrorText(), "Epic sadface: Username is required");
+        assertEquals(loginPage.getErrorText(), "Epic sadface: Password is required");
+    }
+
+    @Test
+    public void checkCapsLockLogin() {
+        loginPage.open();
+        loginPage.login("Standard_user","secret_sauce");
+
+        assertTrue(loginPage.isErrorDisplayed());
+        assertEquals(loginPage.getErrorText(), "Epic sadface: Username and password do not match any user in this service");
+    }
+
+    @Test
+    public void checkLockedUser() {
+        loginPage.open();
+        loginPage.login("locked_out_user","secret_sauce");
+
+        assertTrue(loginPage.isErrorDisplayed());
+        assertEquals(loginPage.getErrorText(), "Epic sadface: Sorry, this user has been locked out.");
     }
 }
